d2dffd43981727122e4a674d2a49c06438990b4e
Abhay Tiwari <emailatabhay@gmail.com>
2024-11-01 13:46:08 +0530

    Created Login System / CSS Not Working

---
+++ dashboard/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def dashboard(request):
+    if request.user.is_customer:
+        return render(request, 'dashboard/customer_dashboard.html')
+
